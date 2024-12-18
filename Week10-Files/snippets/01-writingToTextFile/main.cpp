ec40489544f7c017f17f3f26bc524b8ba239d8b5
pvarna <peter.kolev2001@gmail.com>
2024-12-18 23:50:19 +0200

    add code snippets for Week 10

---
+++ Week10-Files/snippets/01-writingToTextFile/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream> // you need this if you want to work with files
+
+int main ()
+{
+    // o - output
+    // f - file
+    // stream
+    std::ofstream tempFile("tempInfo.txt"); 
+    if (!tempFile.is_open()) // always check if the opening is successfull
+    {
+        std::cout << "Problem while opening the file" << std::endl;
+        return 1;
+    }
+
+    tempFile << "New info\n"; // writng a string
+    tempFile << 5 + 7 << "\n"; // writing a number
+
+    tempFile.close(); // always close the streams
+
+    std::ofstream importantFile("importantInfo.txt", std::ios::app); // always add the flag `app` (append) when you don't want to lose the old information
+    if (!importantFile) // other way to check if the opening is successfull
+    {
+        std::cout << "Problem while opening the file" << std::endl;
+        return 1;
+    }
+
+    importantFile << "I am not the first line of the file\n";
+
+    importantFile.close();
+
+    return 0;
+}
